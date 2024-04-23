ff0be5b1537a93ab8c32ad8f88fa596c4d223baa
Alexandre Souza <allexandre.souza2307@gmail.com>
2024-04-23 18:38:08 -0300

    Criação do projeto de models

---
+++ PicPaySimplificado.Communication/Requests/TransactionRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicPaySimplificado.Communication.Requests
+{
+    public class TransactionRequest
+    {
+        public Guid Id { get; set; }
+        public UserRequest Payee { get; set; }
+        public UserRequest Payer { get; set; }
+        public double Amount { get; set; }
+        public DateTime DateTransaction { get; set; }
+    }
+}
---
+++ PicPaySimplificado.Communication/Requests/WalletRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicPaySimplificado.Communication.Requests
+{
+    public class WalletRequest
+    {
+        public Guid Id { get; set; }
+        public double Balance { get; set; }
+
+    }
+}
