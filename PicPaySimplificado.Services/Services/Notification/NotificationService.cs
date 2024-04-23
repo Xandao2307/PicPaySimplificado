df16151e001e41bd6f0379406b72ab4adfc7c609
Alexandre Souza <allexandre.souza2307@gmail.com>
2024-04-23 18:38:59 -0300

    Inicio da serviço de notificação

---
+++ PicPaySimplificado.Services/Services/Notification/NotificationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PicPaySimplificado.Communication.Requests;
+
+namespace PicPaySimplificado.Services.Services.Notification
+{
+    public class NotificationService
+    {
+        private string _urlMock = "https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6";
+
+        public async Task NotifyAll(TransactionRequest transaction)
+        {
+
+        }
+    }
+}
